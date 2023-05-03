3c18f3842880f9744929266121be56e531f68963
Avery Kyle Arenas <kyruken@gmail.com>
2023-05-02 17:45:30 -0700

    test yelp api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState()
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer wUbMmmP57UL_e5AaOX5X2ZTjDrkWwMt-vjedpjqRKxqmAdNv8MJ_RTDykcIMJ2ehntGfJbk1-0_uY2DVRMkOwuSJvO5Q1-PiGzMLJhB4KJhT-gNpzMJzWtecs05RZHYx'
+    }
+  };
+  
+  async function getData() {
+    fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=nyc&sort_by=best_match&limit=20', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Yelp for couples</h1>
+      <button onClick={getData}>Get data</button>
+      {data}
     </div>
   );
 }
